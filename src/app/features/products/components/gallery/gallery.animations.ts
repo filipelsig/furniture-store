7f84dd4d6fc68c87fa3fedec48eed24f52adf241
filipelsig <filipelsig@gmail.com>
2019-05-18 17:30:57 +0200

    Added animation to gallery

---
+++ src/app/features/products/components/gallery/gallery.animations.ts
@@ -0,0 +1,22 @@
+import {trigger, query, transition, animate, style, stagger} from '@angular/animations';
+
+export const reveal = trigger('reveal', [
+  transition(':enter', [
+    query(
+      'img',
+      [
+        style({opacity: 0, transform: 'translateY(12px)'}),
+        stagger(
+          '150ms',
+          animate(
+            '300ms cubic-bezier(0.4, 0.0, 0.2, 1)',
+            style({opacity: 1, transform: 'translateY(0)'})
+          )
+        )
+      ],
+      {
+        optional: true
+      }
+    )
+  ])
+]);
--- src/app/features/products/components/gallery/gallery.component.ts
+++ src/app/features/products/components/gallery/gallery.component.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {LightboxComponent} from './lightbox/lightbox.component';
 import {MatDialog} from '@angular/material';
+import {reveal} from './gallery.animations';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  animations: [reveal]
 })
 export class GalleryComponent implements OnInit {
   constructor(public dialog: MatDialog) {}
