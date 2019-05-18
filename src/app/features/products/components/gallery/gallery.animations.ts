145d36800422b2ddfdb3a19af5634985391c956d
filipelsig <filipelsig@gmail.com>
2019-05-18 17:33:25 +0200

    Adjusted animation speed

--- src/app/features/products/components/gallery/gallery.animations.ts
+++ src/app/features/products/components/gallery/gallery.animations.ts
@@ -7,7 +7,7 @@ export const reveal = trigger('reveal', [
       [
         style({opacity: 0, transform: 'translateY(12px)'}),
         stagger(
-          '150ms',
+          '100ms',
           animate(
             '300ms cubic-bezier(0.4, 0.0, 0.2, 1)',
             style({opacity: 1, transform: 'translateY(0)'})
