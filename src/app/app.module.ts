71830e93a1a2be1c00729c6d7ec6facb6b743860
filipelsig <filipelsig@gmail.com>
2019-05-20 20:46:05 +0200

    Fixed issues with SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
+import {PrebootModule} from 'preboot';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), AppRoutingModule, BrowserAnimationsModule, CoreModule],
+  imports: [
+    PrebootModule.withConfig({appRoot: 'app-root', disableOverlay: false}),
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
