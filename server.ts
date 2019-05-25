2b92eeb14c72f3ba492b7bd0ffd43659f61987cc
filipelsig <40270737+filipelsig@users.noreply.github.com>
2019-05-25 08:08:24 +0200

    Added compression

--- server.ts
+++ server.ts
@@ -11,6 +11,8 @@ import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
 import * as express from 'express';
 import {join} from 'path';
 
+import * as compression from 'compression';
+
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
@@ -22,6 +24,8 @@ const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
+
+app.use(compression()); // Compress response to client
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
